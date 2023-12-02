ef57fc63a3162ccbf3af3e6ccf785959fbe32650
Casne99 <luca.casnedi@protonmail.com>
2023-12-02 13:14:21 +0100

    Added day2 part 2 solution

---
+++ src/day2/part2.groovy
@@ -0,0 +1,64 @@
+int ans = 0
+
+new File('input.txt').readLines()
+        .each { it ->
+            def gameData = it.split(':')[1].trim()
+            def extractions = []
+            gameData.split(';').each { extraction ->
+                extractions.add(new Extraction(extraction as String))
+            }
+            ans += new Game(extractions).computePower()
+        }
+
+println "Answer is ${ans}"
+
+
+class Extraction {
+
+    private final Map<String, Integer> occurrences = [
+            'red'  : 0,
+            'green': 0,
+            'blue' : 0
+    ]
+
+    Extraction(final String toParse) {
+        toParse.split(',').each { extraction ->
+            String[] toInsert = (extraction as String).trim().split(' ')
+
+            String key = toInsert[1]
+            Integer toAdd = toInsert[0] as Integer
+            Integer alreadyPresent = occurrences[key]
+
+            occurrences[key] = alreadyPresent + toAdd
+        }
+    }
+
+    int getValue(final String color) {
+        occurrences[color]
+    }
+}
+
+class Game {
+
+    private final List<Extraction> extractions = []
+
+    Game(final List<Extraction> extractions) {
+        extractions.each { it ->
+            this.extractions.add(it)
+        }
+    }
+
+    private int getMin(final String color) {
+        extractions.collect(extraction ->
+            extraction.getValue(color) as Integer
+        ).max()
+    }
+
+    int computePower() {
+        def mins = ['red', 'green', 'blue'].collect( it -> getMin(it))
+        println(mins)
+        mins.inject(1) { product, next ->
+            product * next
+        }
+    }
+}
